28c4eaf94d90118d86d33f2e663744072b72f35b
franco <franco@franco>
2020-10-31 00:13:28 -0300

    Lógica de inserción de vecinos corregida
    Ahora los vértices de las aristas insertadas tienen a sus vecinos
    correspondientes

--- clusteringHumano/src/juego/GrafoTest.java
+++ clusteringHumano/src/juego/GrafoTest.java
@@ -2,29 +2,29 @@ package juego;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 class GrafoTest {
 
-	@Test
-	void creacionDeGrafoTest() {
-		Grafo g = new Grafo();
-	}
-
-	@Test
-	void creacionDeVerticeTest() {
-		Vertice vertice = new Vertice(new Persona(1, 2, 1, 5));
-
-	}
+//	@Before		NO SE COMO SE USA ESTO
+//	public void initialize() {
+//		Persona i = new Persona(1, 2, 1, 5);
+//		Persona j = new Persona(3, 3, 2, 4);
+//		Vertice v1 = new Vertice(i);
+//		Vertice v2 = new Vertice(j);
+//		Arista a = new Arista(v1, v2);
+//	}
 	
 	@Test
-	void creacionDeAristaTest() {
+	public void insercionDeVecinosTest() {
+		Persona i = new Persona(1, 2, 1, 5);
+		Persona j = new Persona(3, 3, 2, 4);
+		Vertice v1 = new Vertice(i);
+		Vertice v2 = new Vertice(j);
+		Arista a = new Arista(v1, v2);
 		
-	}
-	
-	@Test
-	void insercionDeArista() {
-		
+		assertTrue(a.getVertice1().getVecinos().contains(a.getVertice2()));
 	}
 	
 }

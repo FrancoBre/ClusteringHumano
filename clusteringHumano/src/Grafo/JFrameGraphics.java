e0e4b3f9ea1ce1b9fe885a798533041fd7636834
Usuario <Usuario@DESKTOP-SJR6E1A>
2020-11-03 23:04:32 -0300

    Agregue un package de Grafo pero es solo de PRUEBA para ir probando

---
+++ clusteringHumano/src/Grafo/JFrameGraphics.java
@@ -0,0 +1,22 @@
+package Grafo;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class JFrameGraphics extends JPanel{
+    public void paint(Graphics g){
+        int xValues[] = {25, 145, 25, 145, 25};
+        int yValues[] = {25, 25, 145, 145, 25};
+        int points = 5;        
+        g.drawPolygon(xValues, yValues, points);
+    }
+    
+    public static void main(String[] args){
+        JFrame frame= new JFrame("JavaTutorial.net");    
+        frame.getContentPane().add(new JFrameGraphics());
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);        
+    }
+}

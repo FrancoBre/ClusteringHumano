397cd757e7248cf667b8bb3d713e19d3252dbfe1
sabri <sabri@DESKTOP-GRUEQNT>
2020-11-06 14:40:17 -0300

    es solo prueba

--- clusteringHumano/src/logica/Arista.java
+++ clusteringHumano/src/logica/Arista.java
@@ -4,7 +4,6 @@ public class Arista {
 	
 	private Vertice vertice1, vertice2;
 	private int peso;
-
 	 public Arista(Vertice vertice1, Vertice vertice2, int peso){
 		this.vertice1=vertice1;
 		this.vertice2=vertice2;

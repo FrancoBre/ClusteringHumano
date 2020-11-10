76576afe1dff1bdebccd97430e452b6fa4c00a9b
franco <bregolif.fb@gmail.com>
2020-11-10 12:38:55 -0300

    Integración de la clase cluster
    Ahora sí

---
+++ clusteringHumano/src/logica/Cluster.java
@@ -0,0 +1,41 @@
+package logica;
+
+import java.util.ArrayList;
+
+//Clase que contiene dos grupos definidos de vertices
+public class Cluster {
+	
+	public ArrayList<Vertice> grupo1;
+	public ArrayList<Vertice> grupo2;
+	
+	public Cluster(Grafo grafo) {
+			
+		if(grafo instanceof GCompleto || grafo instanceof Grafo)
+			grafo = new AGM(grafo);
+		
+		Vertice v1 = Busqueda.aristaDePesoMaximo(grafo).getVertice1();
+		Vertice v2 = Busqueda.aristaDePesoMaximo(grafo).getVertice2();
+			
+		grafo.eliminarArista(Busqueda.aristaDePesoMaximo(grafo));
+			
+		this.grupo1 = (Busqueda.alcanzablesDesde(grafo, v1));
+		this.grupo2 = (Busqueda.alcanzablesDesde(grafo, v2));
+			
+	}
+	
+	public ArrayList<Vertice> getGrupo1() {
+		return grupo1;
+	}
+
+	public void setGrupo1(ArrayList<Vertice> grupo1) {
+		this.grupo1 = grupo1;
+	}
+
+	public ArrayList<Vertice> getGrupo2() {
+		return grupo2;
+	}
+
+	public void setGrupo2(ArrayList<Vertice> grupo2) {
+		this.grupo2 = grupo2;
+	}
+}
